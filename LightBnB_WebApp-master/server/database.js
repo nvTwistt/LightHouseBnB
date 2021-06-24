1c1ab1e5f9030059dab4f06a922d1e2c841bd399
nv_twistt <mtamkee@icloud.com>
2021-06-24 10:23:50 -0700

    finished 02_seeds to populate database with users, then implmented users in database.js so that users can login, stay logged in and so that they can sign up

---
+++ LightBnB_WebApp-master/server/database.js
@@ -0,0 +1,122 @@
+const properties = require('./json/properties.json');
+const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+/// Users
+
+/**
+ * Get a single user from the database given their email.
+ * @param {String} email The email of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithEmail = function(email) {
+  return pool.query(`
+  SELECT *
+  FROM users
+  WHERE email = $1`,
+  [email])
+  .then(res => {
+     const user = res.rows[0];
+     if (user) {
+       return user;
+     } else {
+       return null;
+     }
+  })
+}
+exports.getUserWithEmail = getUserWithEmail;
+
+/**
+ * Get a single user from the database given their id.
+ * @param {string} id The id of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithId = function(id) {
+  return pool.query(`
+  SELECT *
+  FROM users
+  WHERE id = $1`,
+  [id])
+  .then(res => {
+     const user = res.rows[0];
+     if (user) {
+       return user;
+     } else {
+       return null;
+     }
+  })
+}
+exports.getUserWithId = getUserWithId;
+
+
+/**
+ * Add a new user to the database.
+ * @param {{name: string, password: string, email: string}} user
+ * @return {Promise<{}>} A promise to the user.
+ */
+const addUser =  function(user) {
+  let userData = [user.name, user.password, user.email];
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $3, $2)
+  RETURNING *;
+  `, userData)
+  .then(res => {
+    return res.rows[0];
+  })
+}
+exports.addUser = addUser;
+
+/// Reservations
+
+/**
+ * Get all reservations for a single user.
+ * @param {string} guest_id The id of the user.
+ * @return {Promise<[{}]>} A promise to the reservations.
+ */
+const getAllReservations = function(guest_id, limit = 10) {
+  return getAllProperties(null, 2);
+}
+exports.getAllReservations = getAllReservations;
+
+/// Properties
+
+/**
+ * Get all properties.
+ * @param {{}} options An object containing query options.
+ * @param {*} limit The number of results to return.
+ * @return {Promise<[{}]>}  A promise to the properties.
+ */
+const getAllProperties = function(options, limit = 10) {
+  const data = [options, limit]
+  return pool.query(`
+  SELECT *
+  FROM properties
+  LIMIT $2
+  `, data)
+  .then(res => {
+    console.log(res.rows);
+  })
+  .catch((err) => err.message);
+}
+exports.getAllProperties = getAllProperties;
+
+
+/**
+ * Add a property to the database
+ * @param {{}} property An object containing all of the property details.
+ * @return {Promise<{}>} A promise to the property.
+ */
+const addProperty = function(property) {
+  const propertyId = Object.keys(properties).length + 1;
+  property.id = propertyId;
+  properties[propertyId] = property;
+  return Promise.resolve(property);
+}
+exports.addProperty = addProperty;
